1a7eeba67f0598dc1061a8e5c4baa793071f2729
JackAndersYT <JackAnders40@gmail.com>
2025-09-01 00:51:48 -0500

    Cambio para subir proyecto en Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,12 @@ const io = new Server(server, {
     }
 });
 
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true
+};
+app.use(cors(corsOptions));
+
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
